13c572ab9ba49199347fdefde30b95e73b463cec
nnaemekaopara <1513552@rgu.ac.uk>
2016-05-01 09:28:34 +0100

    Update login.php

--- login.php
+++ login.php
@@ -11,8 +11,8 @@
 		}else
 		{
 			// Define $username and $password
-			$username = mysqli_real_escape_string($db, $_POST['username']);
-			$password = md5(mysqli_real_escape_string($db, $_POST['password']));
+			$username = ($db, $_POST['username']);
+			$password = md5($db, $_POST['password']);
 
 
 			
